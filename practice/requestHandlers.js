b06ab48d4c5bdee339f86ae870f9d58aa60de2a9
Ji-Ha <xodhkd36@naver.com>
2021-02-18 20:55:17 +0900

    Handler가 response에 반응하여 처리

--- practice/requestHandlers.js
+++ practice/requestHandlers.js
@@ -1,9 +1,15 @@
-function view() {
+function view(response) {
     console.log('request handler called --> view');
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write('Hello this is view');
+    response.end();
 }
 
-function create() {
+function create(response) {
     console.log('request handler called --> create');
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    response.write('Hello this is create');
+    response.end();
 }
 
 var handle = {};
--- practice/router.js
+++ practice/router.js
@@ -1,12 +1,15 @@
 const { handle } = require("./requestHandlers");
 
-function route(pathname) {
+function route(handle, pathname, response) {
     console.log('about to route a request for ' + pathname);
     
     if(typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        handle[pathname](response);
     } else {
         console.log('no request handler found for ' + pathname);
+        response.writeHead(404, {'Content-Type' : 'text/plain'});
+        response.write('404 Not Found');
+        response.end();
     }
 }
 
