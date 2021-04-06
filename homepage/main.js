d2a022e14afb9deca14416b91a1bbe19d80bbce1
Ji-Ha <xodhkd36@naver.com>
2021-04-06 22:57:47 +0900

    검색창 만들기 설정

--- homepage/main.js
+++ homepage/main.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
 
 var app = http.createServer(function(request, response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);;
+    var url = request.url;
+    if(request.url == '/'){
+        url = '/s.html';
+    }
+    if(request.url == '/favicon.ico'){
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
     
 });
+
+app.listen(3000);
