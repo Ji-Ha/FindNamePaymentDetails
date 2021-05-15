4ec932f1e2f209ff00edb7b9bd3ec8e499656a13
Ji-Ha <xodhkd36@naver.com>
2021-05-15 22:17:51 +0900

    retry

--- practice/index.js
+++ practice/index.js
@@ -1,22 +1,59 @@
 var express = require('express');
-var 
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://xodhkd36:9872@cluster0.0a6bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
 
-app.get('/hello', function(req, res){
-    res.render('hello', {name:req.query.nameQuery});
+// DB schema // 4
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
 });
+var Problem = mongoose.model('contact', contactSchema); // 5
 
-app.get('/hello/:nameParam', function(req, res){
-    res.render('hello', {name:req.params.nameParam});
+// Routes
+// Home // 6
+app.get('/', function(req, res){
+  res.redirect('/contacts');
 });
-
-app.get('/problem', function(req, res){
-    res.send('문제가 보이나요 ? ');
+// Contacts - Index // 7
+app.get('/contacts', function(req, res){
+  Problem.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
 });
-
+// Contacts - New // 8
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// Contacts - create // 9
+app.post('/contacts', function(req, res){
+  Problem.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
 var port = 3000;
 
 app.listen(port, function(){
